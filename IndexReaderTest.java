30f25caf675b4f2e1c88f778f2285930fca879fc
konradilczuk <dokken5@wp.pl>
2012-09-11 20:24:07 +0200

    No changes
    testing

--- IndexReaderTest.java
+++ IndexReaderTest.java
@@ -5,19 +5,18 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
-
 public class IndexReaderTest {
 
 	@Test
 	public void testOfIndexReader() {
-		
+
 		IndexReader inReader = new IndexReader(Paths.get("wordIndex"));
-		
-		String[] strarr = {"apple", "blue", "green", "zebra"};
+		String[] arr = {"apple","blue", "green", "zebra"};
 		try {
-			for(int x=0; x<strarr.length;x++){
-			
-			assertEquals(strarr[x], inReader.readUTF());
+			String x;
+			for(String word: arr){
+				x = inReader.readUTF();
+				assertEquals(x,word, x);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
