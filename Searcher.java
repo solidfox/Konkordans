0e3d27c32d67b4b3df11fba856bda45a28ffcda4
Daniel Schlaug <daniel.schlaug@gmail.com>
2012-09-12 10:10:31 +0200

    Fixes on Searcher

--- Searcher.java
+++ Searcher.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,16 +50,29 @@ public class Searcher {
 		System.out.println(trieRoot.size());
 	}
 	
+	/**
+	 * Returns the first three letters in a given string.
+	 * Or, if the string is shorter than 3 letters, returns the string unmodified.
+	 * @param word the word to get the truplet for.
+	 * @return the first three letters in a given string. 
+	 * Or, if the string is shorter than 3 letters, returns the string unmodified.
+	 */
 	private String getTruplet(String word) {
-		int trupletEnd = (word.length() < 3 ? word.length() - 1 : 2);
+		int trupletEnd = (word.length() < 3 ? word.length() : 3);
 		String truplet = word.substring(0, trupletEnd);
 		return truplet;
 	}
 	
 	public String findWord(String word) throws IOException {
+		// Open the korpus file
 		IndexReader korpus = new IndexReader(Paths.get("korpus"));
-		long wordIndexPointer = this.trieRoot.get(this.getTruplet(word));
-		long[] instancePointers = getInstancePointers(wordIndexPointer);
+		// Get the truplet pointer
+		long trupletPointer = this.trieRoot.get(this.getTruplet(word));
+		// Find the word in the wordIndex
+		long instanceIndexPointer = this.findInstanceIndexPointer(trupletPointer, word);
+		// Fetch instance pointers for word
+		long[] instancePointers = readInstancePointers(instanceIndexPointer);
+		Arrays.sort(instancePointers);
 		StringBuilder out = new StringBuilder(instancePointers.length * 50);
 		long lastPointer = 0;
 		for (long pointer : instancePointers) {
@@ -74,10 +88,38 @@ public class Searcher {
 		return out.toString();
 	}
 	
-	private long[] getInstancePointers(long wordIndexPointer) {
+	/**
+	 * Returns -1 if no word was found.
+	 * @param wordIndexPointer
+	 * @param word
+	 * @return
+	 */
+	private long findInstanceIndexPointer(long wordIndexPointer, String word) {
+		IndexReader wordIndexReader = new IndexReader(wordIndexFile);
+		try {
+			wordIndexReader.skip(wordIndexPointer);
+			// Read in first word
+			String currentWord = wordIndexReader.readUTF();
+			long currentPointer = wordIndexReader.readLong();
+			while (!currentWord.equals(word)) {
+				System.out.println(currentWord);
+				// Make sure we don't search to far
+				if (!currentWord.startsWith(this.getTruplet(word))) {
+					return -1;
+				}
+				currentWord = wordIndexReader.readUTF();
+				currentPointer = wordIndexReader.readLong();
+			}
+			return currentPointer;
+		} catch (IOException e) {
+			return -1;
+		}
+	}
+	
+	private long[] readInstancePointers(long instanceIndexPointer) {
 		try {
 			RandomAccessFile wordIndex = new RandomAccessFile("instanceIndex", "r");
-			wordIndex.seek(wordIndexPointer);
+			wordIndex.seek(instanceIndexPointer);
 			int instanceCount = wordIndex.readInt();
 			long[] instancePointers = new long[instanceCount];
 			for (int i = 0; i < instanceCount; i++) {
